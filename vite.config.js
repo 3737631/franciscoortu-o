b9f220031808153cec90b37be2f2e3cf31c39156
Paquito <paquito@example.com>
2026-06-21 02:23:56 +0200

    fix: quitar crossorigin, forzar redeploy

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/franciscoortu-o/',
   build: { outDir: 'docs' },
+  plugins: [
+    {
+      name: 'strip-crossorigin',
+      transformIndexHtml(html) {
+        return html.replaceAll('crossorigin', '')
+      },
+    },
+  ],
 })
